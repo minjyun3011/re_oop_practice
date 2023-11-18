0db5b31b7ba051486e2b44263dc22a528291fa83
minjyun3011 <grand3canyon10@gmail.com>
2023-11-18 19:11:37 +0900

    残りのルール 文字種はアルファベット小文字のみ

--- username.py
+++ username.py
@@ -1,10 +1,12 @@
 # クラス(データ型)の定義
 class UserName:
     def __init__(self, name):
-        #もしnameの文字数が4文字以上20文字以下でなければ
+        #もしnameの文字数が3文字以上20文字以下でなければ
         if not (3 <= len(name) <= 20):
             # エラー文を表示して終了する
             raise ValueError(f"{name}はルール違反です")
+        if not name.islower():
+            raise ValueError(f"{name}はアルファベットの小文字である必要があります")
         self.name = name
 
     def screen_name(self, new_name):
@@ -12,12 +14,17 @@ class UserName:
         self.name = new_name.upper()
         return self.name
 
+
 # UserNameクラスのインスタンス化
 tanaka = UserName(name="tanaka")
 bob = UserName(name="bob")
+sato = UserName(name="sato")
+
 # 出力
 print(tanaka.name)
 print(bob.name)
+print(sato.name)
+
 
 # tanaka.screen_name("tanaka")
 print(tanaka.screen_name("tanaka"))
